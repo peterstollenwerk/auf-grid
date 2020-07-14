186c094a40097f2fd7eb5ba6692e0bef3d230f07
PeterStollenwerk <ps@peterstollenwerk.de>
2020-07-14 19:14:31 +0200

    added outset fields

--- src/components/fields/GridSettingsField.vue
+++ src/components/fields/GridSettingsField.vue
@@ -7,12 +7,12 @@
 
   <k-fieldset v-model="grid_settings" @input="input" :fields="{
     grid_column_preset: {
-      label: 'Grid Item Column Preset',
+      label: 'Column Preset',
       type: 'grid_column_preset',
       help: 'select »custom...«-preset for custom start- and end-column'
     },
     grid_column_start_class: {
-      label: 'Grid Item Column Start Class',
+      label: 'Column Start Class',
       type: 'grid_column_start_class',
       classes: start_classes,
       width: '1/2',
@@ -21,7 +21,7 @@
       }
     },
     grid_column_end_class: {
-      label: 'Grid Item Column End Class',
+      label: 'Column End Class',
       type: 'grid_column_end_class',
       classes: end_classes,
       width: '1/2',
@@ -30,16 +30,28 @@
       }
     },
     grid_justify_self_class: {
-      label: 'Grid Item Justify Self Class',
+      label: 'Justify Self Class',
       type: 'grid_justify_self',
       width: '1/2',
       help: 'horizontal alignment'
     },
     grid_align_self_class: {
-      label: 'Grid Item Align Self Class',
+      label: 'Align Self Class',
       type: 'grid_align_self',
       width: '1/2',
       help: 'vertical alignment'
+    },
+    grid_outset_left: {
+      label: 'Outset Left',
+      type: 'toggle',
+      width: '1/2',
+      help: 'remove left column gap'
+    },
+    grid_outset_right: {
+      label: 'Outset Right',
+      type: 'toggle',
+      width: '1/2',
+      help: 'remove right column gap'
     }
   }" />
 
