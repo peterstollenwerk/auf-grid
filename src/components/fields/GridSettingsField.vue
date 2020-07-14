154468cb007d3b7f2a2d6db0db97aadcc4a4142c
PeterStollenwerk <ps@peterstollenwerk.de>
2020-07-14 21:59:28 +0200

    grid settingsField nice

--- src/components/fields/GridSettingsField.vue
+++ src/components/fields/GridSettingsField.vue
@@ -9,15 +9,15 @@
     grid_column_preset: {
       label: 'Column Preset',
       type: 'grid_column_preset',
-      help: 'select »custom...«-preset for custom start- and end-column'
+      help: 'select »custom...« for custom start- and end-column',
     },
     grid_column_start_class: {
       label: 'Column Start Class',
       type: 'grid_column_start_class',
       classes: start_classes,
       width: '1/2',
-      when: {
-        grid_column_preset: 'grid__column--custom'
+      when: { 
+        grid_column_preset: 'grid__column--custom' 
       }
     },
     grid_column_end_class: {
@@ -25,33 +25,66 @@
       type: 'grid_column_end_class',
       classes: end_classes,
       width: '1/2',
-      when: {
-        grid_column_preset: 'grid__column--custom'
+      when: { 
+        grid_column_preset: 'grid__column--custom' 
       }
+    },
+    grid_settings_devider_1: {
+      type: 'line',
+      when: { 
+        grid_column_preset: 'grid__column--custom' 
+      },
+    },
+    grid_expert_mode_toggle: {
+      label: 'Expert Mode',
+      type: 'toggle',
     },
     grid_justify_self_class: {
       label: 'Justify Self Class',
       type: 'grid_justify_self',
       width: '1/2',
-      help: 'horizontal alignment'
+      help: 'horizontal alignment',
+      when: { grid_expert_mode_toggle: true }
     },
     grid_align_self_class: {
       label: 'Align Self Class',
       type: 'grid_align_self',
       width: '1/2',
-      help: 'vertical alignment'
+      help: 'vertical alignment',
+      when: { grid_expert_mode_toggle: true }
     },
     grid_outset_left: {
       label: 'Outset Left',
       type: 'toggle',
       width: '1/2',
-      help: 'remove left column gap'
+      help: 'remove left column gap',
+      when: { grid_expert_mode_toggle: true }
     },
     grid_outset_right: {
       label: 'Outset Right',
       type: 'toggle',
       width: '1/2',
-      help: 'remove right column gap'
+      help: 'remove right column gap',
+      when: { grid_expert_mode_toggle: true }
+    },
+    grid_margin_top: {
+      label: 'Margin Top',
+      type: 'number',
+      width: '2/4',
+      after: 'rem',
+      help: 'add extra space between items or...',
+      when: { grid_expert_mode_toggle: true }
+    },
+    grid_margin_bottom: {
+      label: 'Margin Bottom',
+      type: 'number',
+      width: '2/4',
+      after: 'rem',
+      help: '... use negative margins to make them overlap',
+      when: { grid_expert_mode_toggle: true }
+    },
+    grid_settings_devider: {
+      type: 'line'
     }
   }" />
 
@@ -88,6 +121,11 @@ export default {
     },
     input() {
       console.log(this.grid_settings);
+
+      if(this.grid_settings.grid_expert_mode_toggle === false) {
+        this.grid_settings = {}
+      }
+
       this.$emit("input", JSON.stringify(this.grid_settings, function(key, value){
         // only store actual values
         if (value.length < 1) { return undefined; }
