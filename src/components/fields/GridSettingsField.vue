4a7965f365b31e6561a30cebb3dfaada6b7764f4
PeterStollenwerk <ps@peterstollenwerk.de>
2020-08-31 22:49:43 +0200

    bug: auf-grid settings expert mode off triggered full reset. solved

--- src/components/fields/GridSettingsField.vue
+++ src/components/fields/GridSettingsField.vue
@@ -13,73 +13,43 @@
       type: 'grid_column_start_class',
       classes: start_classes,
       width: '1/2',
-      when: { 
-        grid_column_preset: 'grid__column--custom' 
-      }
+      when: { grid_column_preset: 'grid__column--custom' }
+
     },
     grid_column_end_class: {
       label: 'Column End Class',
       type: 'grid_column_end_class',
       classes: end_classes,
       width: '1/2',
-      when: { 
-        grid_column_preset: 'grid__column--custom' 
-      }
+      when: { grid_column_preset: 'grid__column--custom' }
     },
-    grid_settings_divider: {
-      type: 'line',
-      when: { 
-        grid_column_preset: 'grid__column--custom' 
-      },
-    },
-    grid_expert_mode_toggle: {
-      label: 'Expert Mode',
-      type: 'toggle',
+    grid_column_end_class_devider: { 
+      type: 'line'
     },
     grid_justify_self_class: {
       label: 'Justify Self Class',
       type: 'grid_justify_self',
       width: '1/2',
       help: 'horizontal alignment',
-      when: { grid_expert_mode_toggle: true }
     },
     grid_align_self_class: {
       label: 'Align Self Class',
       type: 'grid_align_self',
       width: '1/2',
       help: 'vertical alignment',
-      when: { grid_expert_mode_toggle: true }
     },
     grid_outset_left: {
       label: 'Outset Left',
       type: 'toggle',
       width: '1/2',
       help: 'remove left column gap',
-      when: { grid_expert_mode_toggle: true }
     },
     grid_outset_right: {
       label: 'Outset Right',
       type: 'toggle',
       width: '1/2',
       help: 'remove right column gap',
-      when: { grid_expert_mode_toggle: true }
     },
-    grid_margin_top: {
-      label: 'Margin Top',
-      type: 'number',
-      width: '2/4',
-      after: 'rem',
-      help: 'add extra space between items or...',
-      when: { grid_expert_mode_toggle: true }
-    },
-    grid_margin_bottom: {
-      label: 'Margin Bottom',
-      type: 'number',
-      width: '2/4',
-      after: 'rem',
-      help: '... use negative margins to make them overlap',
-      when: { grid_expert_mode_toggle: true }
-    }
   }" />
 
   </div>
@@ -114,10 +84,6 @@ export default {
       }
     },
     input() {
-      if(this.grid_settings.grid_expert_mode_toggle === false) {
-        this.grid_settings = {}
-      }
-
       this.$emit("input", JSON.stringify(this.grid_settings, function(key, value){
         // only store actual values
         if (value.length < 1) { return undefined; }
