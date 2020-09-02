970b9fcc6a73fdf83d513c376c0eab01bce9a4ce
PeterStollenwerk <ps@peterstollenwerk.de>
2020-09-02 21:54:23 +0200

    optimized GridSettingsField

--- src/components/fields/GridSettingsField.vue
+++ src/components/fields/GridSettingsField.vue
@@ -8,13 +8,13 @@
       type: 'grid_column_preset',
       help: 'select »custom...« for custom start- and end-column',
     },
+
     grid_column_start_class: {
       label: 'Column Start Class',
       type: 'grid_column_start_class',
       classes: start_classes,
       width: '1/2',
       when: { grid_column_preset: 'grid__column--custom' }
-
     },
     grid_column_end_class: {
       label: 'Column End Class',
@@ -23,31 +23,28 @@
       width: '1/2',
       when: { grid_column_preset: 'grid__column--custom' }
     },
-    grid_column_end_class_devider: { 
-      type: 'line'
-    },
     grid_justify_self_class: {
       label: 'Justify Self Class',
       type: 'grid_justify_self',
-      width: '1/2',
+      width: '1/4',
       help: 'horizontal alignment',
     },
     grid_align_self_class: {
       label: 'Align Self Class',
       type: 'grid_align_self',
-      width: '1/2',
+      width: '1/4',
       help: 'vertical alignment',
     },
     grid_outset_left: {
       label: 'Outset Left',
       type: 'toggle',
-      width: '1/2',
+      width: '1/4',
       help: 'remove left column gap',
     },
     grid_outset_right: {
       label: 'Outset Right',
       type: 'toggle',
-      width: '1/2',
+      width: '1/4',
       help: 'remove right column gap',
     },
   }" />
